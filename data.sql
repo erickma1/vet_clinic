e59cc0e26c3b85d28ecdf59cbe5a53ebaf8bf3db
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-10-28 10:13:58 +0000

    Update data.sql

--- data.sql
+++ data.sql
@@ -9,8 +9,8 @@ VALUES
    ('Devimon', '2017-05-12', 11, true, 5);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg) VALUES 
-  ('Charmandor', '2020-02-08', 0, FALSE, -11),
-  ('Plantmon','2021-11-15', 2, TRUE, 5.7),
+  ('Charmander', '2020-02-08', 0, FALSE, -11),
+  ('Plantmon','2021-11-15', 2, TRUE, -5.7),
   ('Squirtle', '1993-04-02', 3, FALSE, -12.13),
   ('Angemon', '2005-06-12', 1, TRUE, -45),
   ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
